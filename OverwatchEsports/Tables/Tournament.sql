3c40c4562a9ef6af055418cab71b039cdf458da6
Louison Hernoux <lou051093@gmail.com>
2019-09-05 11:00:50 +0200

    Changed Tournament image_url to ImageURL

--- OverwatchEsports/Tables/Tournament.sql
+++ OverwatchEsports/Tables/Tournament.sql
@@ -4,7 +4,7 @@
     [LeagueName] NVARCHAR(50) NOT NULL, 
     [SeriesFullName] NVARCHAR(50) NOT NULL, 
     [Name] NVARCHAR(50) NOT NULL, 
-    [image_url] NVARCHAR(MAX) NULL, 
+    [ImageURL] NVARCHAR(MAX) NULL, 
     [BeginDate] DATETIME2 NULL, 
     [EndDate] DATETIME2 NULL, 
     [Prizepool] INT NULL
