56204bf721ee0c4143061e7df927143e38397895
Louison Hernoux <lou051093@gmail.com>
2019-09-09 08:37:09 +0200

    DAL PlayerService

---
+++ DAL_Esports_Global/Service/PlayerService.cs
@@ -0,0 +1,38 @@
+﻿using DAL_Esports_Global.Data;
+using DAL_Esports_Global.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_Esports_Global.Service
+{
+    class PlayerService : IRepository<Player, int>
+    {
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Player> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Player Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(Player entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Player entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
