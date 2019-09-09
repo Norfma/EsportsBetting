cf8c4c8ab5f6344cf636c578911a2cb9c97c5467
Louison Hernoux <lou051093@gmail.com>
2019-09-09 08:35:47 +0200

    UpdatePlayer & UpdateTeam stored procedures

---
+++ OverwatchEsports/StoredProcedures/UpdatePlayer.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[UpdatePlayer]
+	@PlayerId int = 0,
+	@Name nvarchar(50),
+	@TeamId int,
+	@ImageURL nvarchar(max)
+AS
+	Update Player set [Name] = @Name, [TeamId] = @TeamId, [ImageURL] = @ImageURL where Id = @PlayerId
+RETURN 0
---
+++ OverwatchEsports/StoredProcedures/UpdateTeam.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[UpdateTeam]
+	@Id int = 0,
+	@Name nvarchar(50),
+	@Acronym nvarchar(10),
+	@ImageURL nvarchar(max)
+AS
+	Update Team set [Name] = @Name, Acronym = @Acronym, ImageURL = @ImageURL where Id = @Id
+RETURN 0
