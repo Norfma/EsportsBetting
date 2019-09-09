12eac5ce3585e1ace981f59f1afe4c50c7fc3221
Louison Hernoux <lou051093@gmail.com>
2019-09-09 08:36:35 +0200

    Dal setup

---
+++ DAL_Esports_Global/DBConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_Esports_Global
+{
+    public static class DBConfig
+    {
+        public const string CONNSTRING = @"Data Source=ZBOOK-4935\SQLEXPRESS;Initial Catalog=EsportsBetting;Persist Security Info=True;User ID=sa;Password=formation";
+    }
+}
+    
