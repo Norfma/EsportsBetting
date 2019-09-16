5a2da4d6c243fa9fbbdd377ef175146bf783195b
Louison Hernoux <lou051093@gmail.com>
2019-09-16 12:58:26 +0200

    Add web api setup project

---
+++ PandascoreUtils/PandaScoreUtils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PandascoreUtils
+{
+    public static class PandaScoreUtils
+    {
+        public static Uri PandaBaseAddress = new Uri(@"https://api.pandascore.co/ow/}");
+        public static string Token = "vMVq6dhpbhBhTrWUs3GcBVM64n9_WWrAbXOx8NOm_7rsgzN_gVk";
+    }
+}
