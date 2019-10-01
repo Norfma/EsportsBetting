a972073b83abd7154055a31dd00fab99c178eae9
Louison Hernoux <lou051093@gmail.com>
2019-10-01 15:52:07 +0200

    User and bets tables

---
+++ OverwatchEsports/Tables/Bet.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Bet]
+(
+	[UserId] INT, 
+    [MatchId] NCHAR(10), 
+    [BettedWinner] NCHAR(10) NOT NULL,
+	constraint [PK_Bet] Primary Key (UserId, MatchId)
+)
---
+++ OverwatchEsports/Tables/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT IDENTITY, 
+    [Username] NVARCHAR(50) NOT NULL, 
+    [Hashpwd] NVARCHAR(MAX) NOT NULL, 
+    [Salt] NVARCHAR(50) NULL, 
+    CONSTRAINT [PK_User] PRIMARY KEY (Id)
+)
