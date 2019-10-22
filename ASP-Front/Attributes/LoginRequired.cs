bcda3a584487225f80ffc0e8156a2ed06438af10
Louison Hernoux <lou051093@gmail.com>
2019-10-22 13:14:29 +0200

    ASP Start

---
+++ ASP-Front/Attributes/LoginRequired.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP_Front.Attributes
+{
+    public class LoginRequired : System.Web.Mvc.AuthorizeAttribute
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            base.OnAuthorization(filterContext);
+        }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            return base.AuthorizeCore(httpContext);
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            base.HandleUnauthorizedRequest(filterContext);
+        }
+    }
+}
