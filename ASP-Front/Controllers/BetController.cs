7ae49fa0518c32f4ce2250d36e312fbd6574c26b
Louison Hernoux <lou051093@gmail.com>
2019-10-25 14:48:14 +0200

    Bet Controller wip

--- ASP-Front/Controllers/BetController.cs
+++ ASP-Front/Controllers/BetController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using ASP_Front.Attributes;
+using ASP_Front.Infrastructure;
+using DAL_Esports_Global.Services;
+using DALBase.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,10 +13,24 @@ namespace ASP_Front.Controllers
     public class BetController : Controller
     {
         // GET: Bet
+        [LoginRequired]
         public ActionResult Index()
         {
-            //BetService
+            UserService service = new UserService();
+
+            IEnumerable<Bet> bets = service.GetUserBets(UserSession.User.UserID);
+
             return View();
         }
+
+        [LoginRequired]
+        public ActionResult Register()
+        {
+            MatchService service = new MatchService();
+            service.GetAll();
+
+            return View();
+        }
+
     }
 }
---
+++ ASP-Front/Models/ViewModels/Bet/BetRegisterForm.cs
@@ -0,0 +1,36 @@
+﻿using ASP_Front.Infrastructure;
+using DAL_Esports_Global.Services;
+using DALBase.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP_Front.Models.ViewModels.Bet
+{
+    public class BetRegisterForm
+    {
+
+        private List<Match> _matches;
+
+        [HiddenInput(DisplayValue = false)]
+        public int MatchId
+        {
+            get => student.SectionId;
+            set => student.SectionId = value;
+        }
+
+        public BetRegisterForm()
+        {
+        }
+
+        public UserModel SaveToDB()
+        {
+            UserService service = new UserService();
+            service.AddUserBet(UserSession.User.UserID, matchId, winnerId);
+            return user;
+        }
+    }
+}
